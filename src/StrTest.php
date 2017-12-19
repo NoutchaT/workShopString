1b7889c45cd9f2e348ae55cbd0ffb1a1103c76c9
NoutchaT <noucha.tchatat@devinci.fr>
2017-12-19 14:38:04 +0100

    Exo 1

--- src/StrTest.php
+++ src/StrTest.php
@@ -10,6 +10,7 @@ class StrTest extends TestCase
     public function testExo1()
     {
         $string = (string) Str::on('my_string')->replace('_',' ');
+        
         $this->assertEquals('my_string', $string);
     }
 }
